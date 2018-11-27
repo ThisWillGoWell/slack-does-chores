e5fc937caada3f7ed77847456b6b0a446bfad980
thiswillgowell <william.gowell@gmail.com>
2018-11-27 12:18:22 -0500

    infa code added

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@
 echo $1
 git commit -am "$1"
 git push
-ssh root@williamgowell.com -C "cd ~/slack-does-chores && git pull && ./run.sh"
+ssh root@williamgowell.com 'cd ~/slack-does-chores && git pull && ./run.sh'
