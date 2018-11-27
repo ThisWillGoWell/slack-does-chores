5f540be8d27557e971b47cfbc0101f6980975c7a
William.Gowell <William.Gowell>
2018-11-27 13:06:03 -0500

    infra codes

--- deploy.bat
+++ deploy.bat
@@ -1,3 +1,3 @@
 git commit -am %1s
 git push
-ssh root@williamgowell.com "cd ~/slack-does-chores && git stash && git pull && git stash drop -q && chmod +x run.sh &&./run.sh"
+ssh root@williamgowell.com "cd ~/slack-does-chores && git stash && git pull && git stash drop -q | true && chmod +x run.sh &&./run.sh"
