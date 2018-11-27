27746037a25b5587b7931b36739e5272de67ed5e
William.Gowell <William.Gowell>
2018-11-27 14:56:53 -0500

    nginx dockerfiles

--- etc/get-certs.sh
+++ etc/get-certs.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 docker-compose run --rm letsencrypt \
   letsencrypt certonly --webroot \
-  --email contact@mockstarket.com --agree-tos \
-  -w /var/www/letsencrypt -d mockstarket.com
+  --email will@williamgowell.com --agree-tos \
+  -w /var/www/letsencrypt -d williamgowell.com
 docker-compose kill -s SIGHUP proxy
