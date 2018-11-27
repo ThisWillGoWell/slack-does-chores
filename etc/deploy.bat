b2d5bbca22c7749d141c76866ef2c7bddf527a2b
William.Gowell <William.Gowell>
2018-11-27 14:51:15 -0500

    deploy httpss

--- etc/deploy.bat
+++ etc/deploy.bat
@@ -1,3 +1,3 @@
 git commit -am %1s
 git push
-ssh root@williamgowell.com "cd ~/slack-does-chores && git stash && git pull && git stash drop -q | true && sed -i 's/\r//' *.sh && chmod +x etc/run.sh && ./etc/run.sh"
+ssh root@williamgowell.com "cd ~/slack-does-chores && git stash && git pull && git stash drop -q | true && sed -i 's/\r//' etc/*.sh && chmod +x etc/run.sh && ./etc/run.sh"
